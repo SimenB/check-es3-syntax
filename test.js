ca42954477b2eac3fbe96731f673cc01b06ae79c
Simen Bekkhus <sbekkhus91@gmail.com>
2016-12-18 15:26:42 +0100

    Use jest instead of yarn

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
-/* eslint import/no-extraneous-dependencies: ["error", { "devDependencies": true }]*/
+/* eslint-env jest */
 
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
-import test from 'ava';
 import Promise from 'bluebird';
 import del from 'del';
 import checkEs3Syntax, { checkString } from './check';
@@ -21,74 +20,74 @@ const errorReturn = [
   { count: 11 + EOL.length, value: `: 'name' };${EOL}` }, // A couple of stupid hacks for windows
 ];
 
-test.afterEach(() => del('./*.patch'));
+afterAll(() => del('*.patch'));
 
-test('should return empty array if no errors in files', async t => {
+test.concurrent('should return empty array if no errors in files', async () => {
   const res = await checkEs3Syntax(path.join(fixturesDirectory, 'compatible.js'));
 
-  t.true(res.length === 0);
+  expect(res.length).toBe(0);
 });
 
-test('should return empty array if no files', async t => {
+test.concurrent('should return empty array if no files', async () => {
   const res = await checkEs3Syntax();
 
-  t.true(res.length === 0);
+  expect(res.length).toBe(0);
 });
 
-test('should return undefined if no errors in  string', async t => {
+test.concurrent('should return undefined if no errors in  string', async () => {
   const res = await checkString("var o = { 'class': 'name' }");
 
-  t.true(res == null);
+  expect(res).toBeUndefined();
 });
 
-test('should return error in file', async t => {
+test.concurrent('should return error in file', async () => {
   const res = await checkEs3Syntax(path.join(fixturesDirectory, 'notCompatible.js'));
 
-  t.true(res.length === 1);
-  t.deepEqual(res[0].textDiff, errorReturn);
+  expect(res.length).toBe(1);
+  expect(res[0].textDiff).toEqual(errorReturn);
 });
 
-test('should return error in array of files', async t => {
+test.concurrent('should return error in array of files', async () => {
   const res = await checkEs3Syntax([path.join(fixturesDirectory, 'notCompatible.js')]);
 
-  t.true(res.length === 1);
-  t.deepEqual(res[0].textDiff, errorReturn);
+  expect(res.length).toBe(1);
+  expect(res[0].textDiff).toEqual(errorReturn);
 });
 
-test('should return error in directory', async t => {
+test.concurrent('should return error in directory', async () => {
   const res = await checkEs3Syntax(fixturesDirectory);
 
-  t.true(res.length === 2);
-  t.deepEqual(res[0].textDiff, errorReturn);
-  t.deepEqual(res[1].textDiff, errorReturn);
+  expect(res.length).toBe(2);
+  expect(res[0].textDiff).toEqual(errorReturn);
+  expect(res[1].textDiff).toEqual(errorReturn);
 });
 
-test('should return error in string', async t => {
+test.concurrent('should return error in string', async () => {
   const res = await checkString(`var o = { class: 'name' };${EOL}`);
 
-  t.deepEqual(res.textDiff, errorReturn);
+  expect(res.textDiff).toEqual(errorReturn);
 });
 
-test.serial('should print patch from file to file if error, and enabled', async t => {
+test.concurrent('should print patch from file to file if error, and enabled', async () => {
   await checkEs3Syntax(path.join(fixturesDirectory, 'notCompatible.js'), { savePatchToDisk: true, directory: process.cwd() });
 
   const fileContent = await readFile('./notCompatible.js.patch');
 
-  t.true(fileContent != null);
+  expect(fileContent).not.toBeUndefined();
 });
 
-test.serial('should print patch from string to file if error, and enabled', async t => {
+test('should print patch from string to file if error, and enabled', async () => {
   await checkString("var o = { class: 'name' };\n", { savePatchToDisk: true, directory: process.cwd() });
 
   const fileContent = await readFile('./stringInput.patch');
 
-  t.true(fileContent != null);
+  expect(fileContent).not.toBeUndefined();
 });
 
-test.serial('should print patch from string to file if error, and enabled with custom name', async t => {
+test('should print patch from string to file if error, and enabled with custom name', async () => {
   await checkString("var o = { class: 'name' };\n", { savePatchToDisk: true, directory: process.cwd(), filename: 'meep' });
 
   const fileContent = await readFile('./meep.patch');
 
-  t.true(fileContent != null);
+  expect(fileContent).not.toBeUndefined();
 });
